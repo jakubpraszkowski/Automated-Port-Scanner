09a05b22855f826301ac3a5d9bfdf06969c622af
Jakub Praszkowski <kubapraszkowski@gmail.com>
2023-03-01 18:03:52 +0100

    Instead of check_port() program is using check_port_protocol() to check also protocols and rising exceptions.

--- PortScanner.py
+++ PortScanner.py
@@ -1,4 +1,4 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, error, SOCK_DGRAM
 
 
 class PortScanner:
@@ -7,14 +7,24 @@ class PortScanner:
         self.start_port = start_port
         self.end_port = end_port
 
-    def check_port(self, ip, port):
+    def check_port_protocol(self, ip, port):
         sock = socket(AF_INET, SOCK_STREAM)
-        result = sock.connect_ex((ip, port))
 
-        if result == 0:
-            print("Port {} is open".format(port))
+        try:
+            sock.connect((ip, port))
+            print(f"Port {port} is open and uses TCP protocol")
 
-        sock.close()
+        except error:
+            try:
+                sock = socket(AF_INET, SOCK_DGRAM)
+                sock.connect((ip, port))
+                print(f"Port {port} is open and uses UDP protocol")
+
+            except error:
+                print(f"Cannot determine the network protocol of port: {port}.")
+
+        finally:
+            sock.close()
 
     def scan_ports(self):
         for ip_address in self.ip_range:
@@ -22,4 +32,4 @@ class PortScanner:
             print(f"Ports for {question}:")
 
             for port in range(int(self.start_port), int(self.end_port) + 1):
-                self.check_port(question, port)
+                self.check_port_protocol(question, port)
